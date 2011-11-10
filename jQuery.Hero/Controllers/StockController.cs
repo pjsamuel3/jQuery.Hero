953d5e3ebc75924995458616398537a0c3c691ce
Peter Samuel <pjsamuel3@hotmail.com>
2011-11-10 16:29:12 +0100

    Added MVC Contrib and added new stock controller

---
+++ jQuery.Hero/Controllers/StockController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQuery.Hero.Controllers
+{
+    public class StockController : Controller
+    {
+        //
+        // GET: /Stock/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
